ca12e2b3dbb91c5d52c491291ef97572d2b0b46c
BalluCoder-cyber <balwantahirwar70@gmail.com>
2026-04-27 08:15:22 +0530

    tenant identifier resolver

--- hms-backend/src/main/java/com/mednex/hms_backend/multitenancy/TenantIdentifierResolver.java
+++ hms-backend/src/main/java/com/mednex/hms_backend/multitenancy/TenantIdentifierResolver.java
@@ -1,4 +1,24 @@
 package com.mednex.hms_backend.multitenancy;
 
-public class TenantIdentifierResolver {
+import org.hibernate.context.spi.CurrentTenantIdentifierResolver;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TenantIdentifierResolver implements CurrentTenantIdentifierResolver<String> {
+
+        @Override
+        public String resolveCurrentTenantIdentifier () {
+        String tenant = TenantContext.getCurrentTenant();
+
+        // If no tenant set yet, use the shared schema
+        return (tenant != null && !tenant.isBlank())
+                ? tenant
+                : "public_shared";
+    }
+
+        @Override
+        public boolean validateExistingCurrentSessions () {
+        return true;
+
+    }
 }
